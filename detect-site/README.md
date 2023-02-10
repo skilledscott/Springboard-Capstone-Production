5bd331c629fb741273966903999b7dde3e9a8e77
Scott Gibson <scottchgibson@gmail.com>
2023-02-10 14:31:02 -0800

    Incorporate bulma and style home page.
    Added bulma to the project for styling the webpage. I installed it
    with npm, and created a 'mystyles.scss' file for customizing bulma.
    
    Unsure why, but certain components like columns aren't being rendered
    as I'd expect. It may be due to the way I incorporated bulma with
    scss.

--- detect-site/README.md
+++ detect-site/README.md
@@ -39,6 +39,18 @@ Instead, it will copy all the configuration files and the transitive dependencie
 
 You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
 
+### `npm run css-build`
+
+Run this to build the styles css file from the scss file
+in the src/styles/ directory. This is imported in App.js
+for the bulma styling.
+
+### `npm run css-watch`
+
+Builds the css file like css-build, but puts it in watch
+mode. So any changes saved to mystyles.scss will trigger
+css-build.
+
 ## Learn More
 
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
--- detect-site/src/App.js
+++ detect-site/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/mystyles.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            {/* Hero component */}
+            <div className="hero is-primary">
+                <div class="hero-body">
+                    <p class="title">
+                        Object Detection Capstone Project
+                    </p>
+                    <p class="subtitle">
+                        Created by Scott Gibson
+                    </p>
+                </div>
+            </div>
+
+            {/* left column: image, right column: api output */}
+            <div className='columns is-mobile'>
+                {/* left column */}
+                <div className='column'>
+                    <p className='title'>image column</p>
+                    <button className='button is-link'>Predict</button>
+                </div>
+
+                {/* right column */}
+                <div className='column'>
+                    <p className='title'>api response column</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
