7832f3b03a5ddecde4f8eb6c9f05f87aa7d6f6c8
Scott Gibson <scottchgibson@gmail.com>
2023-03-07 15:57:03 -0800

    Draw image in canvas element, and link canvas to model detect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import * as tf from '@tensorflow/tfjs'
 import * as cocossd from '@tensorflow-models/coco-ssd';
 
@@ -10,34 +10,77 @@ const App = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [model, setModel] = useState(null);
 
+    const canvasRef = useRef(null);
+
+
+    // On page start, load ssd model
     useEffect(() => {
         loadModel().then((model) => {
             setIsLoading(false);
             setModel(model);
         });
-    }, []);
+    }, [])
 
-    useEffect(() => {
-        if (image == null) return;
-        setImageURL(URL.createObjectURL(image));
-    }, [image]);
-
-
+    // Loads and returns ssd model
     const loadModel = async () => {
         const model = await cocossd.load();
         return model;
     }
 
+
+    // Init the canvas on page load by setting the size and setting fill to black
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+
+        // Fill canvas
+        context.fillStyle = '#000000';
+        context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+    }, [])
+
+
+    useEffect(() => {
+        if (imageURL == null) return;
+
+        var img = new Image();
+        img.src = imageURL;
+        img.onload = () => {
+            const canvas = canvasRef.current;
+            const context = canvas.getContext("2d");
+            context.drawImage(img, 0, 0);
+        }
+    }, [imageURL])
+
+
+    // On user image upload, set the imageURL state to point to the created image blob
     const onImageChange = (e) => {
         setImage(e.target.files[0]);
+        setImageURL(URL.createObjectURL(e.target.files[0]));
     }
 
 
+    // On pressing the predict button, call the model to detect
     const handlePredict = () => {
-        const img = document.getElementById('img');
-        model.detect(img).then(predictions => {
-            console.log('prediction: ', predictions);
-        })
+        model.detect(canvasRef.current).then(preds => {
+            console.log('prediction: ', preds);
+            drawBBoxes(preds);
+        });
+    }
+
+
+    const drawBBoxes = (preds) => {
+        const pred = preds[0];
+        const x = pred.bbox[0];
+        const y = pred.bbox[1];
+        const width = pred.bbox[2];
+        const height = pred.bbox[3];
+
+        const canvas = document.getElementById('image-canvas');
+        const ctx = canvas.getContext('2d');
+
+        console.log('here')
+
+        ctx.strokeRect(x, y, width, height);
     }
 
 
@@ -60,9 +103,7 @@ const App = () => {
                 {/* left column - Image Input and User Buttons */}
                 <div className='column'>
                     {/* User Uploaded Image */}
-                    <figure className='image block is-square'>
-                        <img src={imageURL} alt='user' id='img'></img>
-                    </figure>
+                    <canvas ref={canvasRef} width='416' height='416' />
 
                     <div className='file is-warning'>
                         {/* Image File Input */}
